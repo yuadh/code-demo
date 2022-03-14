ccd4410165ab3397bbe313187d8220d9a6959ade
yuadh <1912546219@qq.com>
2022-03-14 20:31:44 +0800

    updata

---
+++ python/basic/str.py
@@ -0,0 +1,17 @@
+import sys
+from PIL import Image
+def test():
+    # age = 3
+    # args = sys.argv
+    # if age >= 18:
+    #     print('your age is', age)
+    #     print('adult')
+    # else:
+    #     print('your age is' ,sys.path)
+    #     print('teenager')
+    im = Image.open('img/50506923.png')
+    print(im.format, im.size, im.mode)
+    im.thumbnail((200, 100))
+    im.save('thumb.jpg', 'JPEG')
+if __name__=='__main__':
+    test()
