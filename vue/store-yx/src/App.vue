a1c40fc333f9affec46204e320406125291e2b06
yuadh <1912546219@qq.com>
2022-05-04 12:44:19 +0800

    yx

--- vue/store-yx/src/App.vue
+++ vue/store-yx/src/App.vue
@@ -1,14 +1,13 @@
 <template>
-   <div class="container">
-    <!-- 修改数据，测试是否持久化 -->
-    App {{$store.state.user.profile.account}}
-    <button @click="$store.commit('user/setUser',{account:'zhousg'})">设置用户信息</button>
-  </div>
+  <yxCarousel/>
 </template>
 
 <script>
+import  yxCarousel from '@/components/yxCarousel.vue'
 export default {
-
+  components: {
+    yxCarousel
+  }
 }
 </script>
 
---
+++ vue/store-yx/src/components/yxCarousel.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="yxCarousel">
+    <ul class="carouselBody">
+      <RouterLink to='/'>
+        <img src=":;">
+      </RouterLink>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
