e76855bfffb129bf2d68c7e947f10e60c4d1e8b9
yuadh <1912546219@qq.com>
2022-07-21 21:03:34 +0800

    react极客园

--- react/react-code-doc/src/utils/request.js
+++ react/react-code-doc/src/utils/request.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 import { Toast } from 'antd-mobile'
-import { getToken, hasToken } from './storage'
+import { getToken, removeToken, setToken } from './storage'
 import history from './history'
+import store from '@/store'
+import { logout, saveToken } from '@/store/actions/login'
 export const baseURL = 'http://geek.itheima.net/v1_0/'
 // import Toast from 'antd-mobile/es/components/toast'
 const instance = axios.create({
@@ -28,14 +30,14 @@ instance.interceptors.request.use(
 // 响应拦截
 instance.interceptors.response.use(
   (res) => res.data,
-  (err) => {
+  async (err) => {
     if (!err.response) {
       Toast.show({
         content: '网络繁忙,请稍后重试',
         duration: 1000,
       })
     }
-    const { response } = err
+    const { response, config } = err
     // 非token类型的其它错误
     if (response.status !== 401) {
       Toast.show({
@@ -45,6 +47,8 @@ instance.interceptors.response.use(
       })
     }
     const { refresh_token } = getToken()
+    console.log(refresh_token + '22223333')
+    console.log(getToken())
     // 如果没有 refresh_token
     if (!refresh_token) {
       history.push({
@@ -55,8 +59,48 @@ instance.interceptors.response.use(
       })
       return Promise.reject(err)
     }
+    // 如果有 refresh_token 发送请求尝试刷新token
+    try {
+      const res = await axios({
+        method: 'put',
+        url: baseURL + 'authorizations',
+        headers: {
+          Authorization: `Bearer ` + refresh_token,
+        },
+      })
+      const tokenInfo = {
+        token: res.data.data.token,
+        refresh_token: refresh_token,
+      }
+      // 保存在 redux
+      store.dispatch(saveToken(tokenInfo))
+      // 保存至本地
+      setToken(tokenInfo)
+      // 重新发一次请求
+      return instance(config)
+    } catch {
+      removeToken()
+      store.dispatch(
+        logout({
+          token: '',
+          refresh_token: '',
+        })
+      )
+      history.push({
+        pathname: '/login',
+        state: {
+          from: history.location.pathname,
+        },
+      })
+      Toast.show({
+        icon: 'fail',
+        content: err.response.data.message,
+        duration: 1000,
+      })
+      return Promise.reject(err)
+    }
 
-    return Promise.reject(err)
+    // return Promise.reject(err)
   }
 )
 
